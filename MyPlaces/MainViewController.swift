47fc49df8e7f49be7014ea2390bf8c735c990364
ikustow <ikustow@yandex.run>
2020-09-30 00:38:55 +0300

    images

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Ilya on 29.09.2020.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let restaurantNames = [
+    "Балкан Гриль", "Бочка", "Вкусные истории",
+    "Дастархан", "Индокитай",
+    "Классик", "Шок"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
