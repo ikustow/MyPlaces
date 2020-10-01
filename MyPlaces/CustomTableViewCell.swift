a1c69aec1cf26b08b17687a12d13bb9e8e4ffaa3
ikustow <ikustow@yandex.run>
2020-10-01 21:29:01 +0300

    app

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Ilya on 01.10.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
